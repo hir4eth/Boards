720a9dc24e363bb6fc52981526089266d49d7b57
Alexander <pwnzwr@icloud.com>
2021-07-11 21:16:52 +0300

    Circle tool added

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -3,13 +3,14 @@ import canvasState from '../store/canvasState';
 import toolState from '../store/toolState';
 import "../styles/toolbar.scss"
 import Brush from '../tools/Brush';
+import { Circle } from '../tools/Circle';
 import { Rect } from '../tools/Rect';
 const Toolbar = () => {
     return (
         <div className="toolbar" >
             <button className="toolbar__btn brush" onClick={()=> toolState.setTool(new Brush(canvasState.canvas))}></button>
             <button className="toolbar__btn rect" onClick={()=> toolState.setTool(new Rect(canvasState.canvas))}></button>
-            <button className="toolbar__btn circle"></button>
+            <button className="toolbar__btn circle" onClick={()=> toolState.setTool(new Circle(canvasState.canvas))}></button>
             <button className="toolbar__btn eraser"></button>
             <button className="toolbar__btn line"></button>
             <input type="color" style={{marginLeft:10}}/>
